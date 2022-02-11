7cee9c2250d4dbda01cb1059d2d92bc1af5bea89
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-02-11 14:06:15 +0100

    c run e esercizio dell'albero

--- c/001_albero.c
+++ c/001_albero.c
@@ -18,9 +18,60 @@ ESERCIZIO n.1 - Stampa dell'ascii art di un albero di natale
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 
 void main(){
-    prinft("Generatore di alberi di natale.\n");
+    /* Titolo programma */
+    printf("Generatore di alberi di natale - Vittorio Lo Mele, 3Ci.\n");
+
+    /* Variabili globali, configurazione del generatore */
+    int maxTreeWidth; // larghezza massima in colonne dell'albero
+    float decorationRandomnessLevel; // quante decorazioni devono essere inserite randomicamente
+    float snowRandomnessLevel; // intensità della neve
+    bool drawGifts; // disegnare i regali sotto l'albero o meno
+
+    /* Input delle impostazioni del generatore */
+    do{
+        printf("Inserisci larghezza massima dell'albero (10 < x < 80): ");
+        scanf("%d", &maxTreeWidth);
+    } while (maxTreeWidth < 10 || maxTreeWidth > 80);
+
+    do{
+        printf("Inserisci la randomizzazione delle decorazioni (0.1 < x < 1.0): ");
+        scanf("%f", &decorationRandomnessLevel);
+    } while (decorationRandomnessLevel < 0.1 || decorationRandomnessLevel > 1.0);
+
+    do{
+        printf("Inserisci l'intensità della neve (0.1 < x < 1.0): ");
+        scanf("%f", &snowRandomnessLevel);
+    } while (snowRandomnessLevel < 0.1 || snowRandomnessLevel > 1.0);
+
+    char tmpGift;
+    do{
+        printf("Disegnare i regali? (N | s): ");
+        scanf(" %c", &tmpGift);
+    } while (tmpGift == '\0');
+    switch (tmpGift)
+    {
+    case 'S':
+        drawGifts = true;
+        break;
+    case 's':
+        drawGifts = true;
+        break;
+    case 'N':
+        drawGifts = false;
+        break;
+    case 'n':
+        drawGifts = false;
+        break;
+    default:
+        drawGifts = false;
+        break;
+    }
+    
+    /* Inizio codice di generazione */
+    // programma da finire
+    
     return;
-    // PROGRAMMA DA FINIRE
 }
