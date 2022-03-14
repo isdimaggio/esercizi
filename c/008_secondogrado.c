5f6a26ce48041298ece48e78b10aeac10b29f5dc
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-03-14 22:40:52 +0100

    equazione di secondo grado

--- c/008_secondogrado.c
+++ c/008_secondogrado.c
@@ -20,16 +20,49 @@ ESERCIZIO n.8 - Risolvere un equazione di secondo grado.
 // #include <math.h>
 #include <stdio.h>
 
-int main(void){
-    int a, b;
+int main(void)
+{
+    int a, b, c;
+    float x1, x2, delta;
     // input
-    printf("EQUAZIONE: x+a-b=0\n");
+    printf("EQUAZIONE: ax^2+bx+c=0\n");
     printf("Inserisci il primo coefficente (a): ");
     scanf("%d", &a);
     printf("Inserisci il secondo coefficente (b): ");
     scanf("%d", &b);
-    printf("DA RISOLVERE: x+%d-%d=0\n", a, b);
-    // output
-    printf("RISULTATO: x=%d\n", -a+b);
+    printf("Inserisci il termine noto (c): ");
+    scanf("%d", &c);
+    printf("DA RISOLVERE: %dx^2+%dx+%d=0\n", a, b, c);
+    if (a == 0)
+    {
+        if ((b == 0) && (c == 0))
+            printf("Equazione inderterminata\n");
+        else if (b == 0)
+            printf("Equazione impossibile\n");
+        else
+        {
+            printf("Equazione di primo grado: ");
+            x1 = -c / b;
+            printf("x= %f \n", x1);
+        }
+    }
+    else
+    {
+        delta = b * b - 4 * a * c;
+        printf("Delta: %f \n", delta);
+        if (delta > 0)
+        {
+            x1 = (-b - sqrt(delta)) / (2 * a);
+            x2 = (-b + sqrt(delta)) / (2 * a);
+            printf("Radici reali e distine: x1=%f e x2=%f \n", x1, x2);
+        }
+        else if (delta == 0)
+        {
+            x1 = (-b) / (2 * a);
+            printf("x=%f \n", x1);
+        }
+        else
+            printf("Equazione impossibile\n");
+    }
     return 0;
 }
