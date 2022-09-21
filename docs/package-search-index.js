8e6b7a25ba4470edc057aa6de284428a44a11abb
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-09-21 18:27:03 +0200

    fix javadoc + license header

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,2 +1,1 @@
-packageSearchIndex = [{"l": "All Packages", "u": "allpackages-index.html"}, {"l": "EsercizioRettangoli"}];
-updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"EsercizioRettangoli"}];updateSearchResults();
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,5 +1,1 @@
-typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
-    "p": "EsercizioRettangoli",
-    "l": "Main"
-}, {"p": "EsercizioRettangoli", "l": "Rettangolo"}];
-updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"EsercizioRettangoli","l":"Main"},{"p":"EsercizioRettangoli","l":"Rettangolo"}];updateSearchResults();
