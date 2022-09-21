0460b1ea03340b157bb2afef7b0988a734684589
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-09-21 18:22:26 +0200

    mv javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,19 @@
+memberSearchIndex = [{"p": "EsercizioRettangoli", "c": "Rettangolo", "l": "calcolaArea()"}, {
+    "p": "EsercizioRettangoli",
+    "c": "Rettangolo",
+    "l": "calcolaPerimetro()"
+}, {"p": "EsercizioRettangoli", "c": "Rettangolo", "l": "getAltezza()"}, {
+    "p": "EsercizioRettangoli",
+    "c": "Rettangolo",
+    "l": "getBase()"
+}, {"p": "EsercizioRettangoli", "c": "Main", "l": "Main()", "u": "%3Cinit%3E()"}, {
+    "p": "EsercizioRettangoli",
+    "c": "Main",
+    "l": "main(String[])",
+    "u": "main(java.lang.String[])"
+}, {"p": "EsercizioRettangoli", "c": "Rettangolo", "l": "setAltezza(float)"}, {
+    "p": "EsercizioRettangoli",
+    "c": "Rettangolo",
+    "l": "setBase(float)"
+}];
+updateSearchResults();
